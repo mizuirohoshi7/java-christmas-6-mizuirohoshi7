1bdcb3f57de41b1ece3bc74a2d45fec5dd1383c0
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-11-14 00:12:45 +0900

    feat(Order): 증정 메뉴

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -5,6 +5,10 @@ import java.util.Map;
 
 public class Order {
 
+    private static final int GIVEAWAY_PRICE = 10000;
+    private static final String NOTHING = "없음";
+    private static final String GIVEAWAY_MENU = "샴페인 1개";
+
     private final Map<Menu, Integer> menuCounts;
 
     public Order(Map<Menu, Integer> menuCounts) {
@@ -15,5 +19,12 @@ public class Order {
         return menuCounts.values().stream()
                 .mapToInt(Integer::intValue)
                 .sum();
+    }
+
+    public String getGiveawayMenu() {
+        if (getPriceBeforeDiscount() < GIVEAWAY_PRICE) {
+            return NOTHING;
+        }
+        return GIVEAWAY_MENU;
     }
 }

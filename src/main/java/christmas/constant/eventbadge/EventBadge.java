1abc7644a93c7bc421a887aea8ff551be2cc614d
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-11-14 00:26:39 +0900

    feat(EventBadge): 별, 트리, 산타

---
+++ src/main/java/christmas/constant/eventbadge/EventBadge.java
@@ -0,0 +1,25 @@
+package christmas.constant.eventbadge;
+
+public enum EventBadge {
+
+    STAR("별", 5000),
+    TREE("트리", 10000),
+    SANTA("산타", 20000)
+    ;
+
+    private final String name;
+    private final int price;
+
+    EventBadge(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}

5e282b84b586b9a4123dbd1092899638d01dd988
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-11-14 17:57:17 +0900

    feat(OutputMessage): 출력 메세지 enum으로 분리

---
+++ src/main/java/christmas/constant/view/OutputMessage.java
@@ -0,0 +1,26 @@
+package christmas.constant.view;
+
+public enum OutputMessage {
+
+    START_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
+    PREVIEW_MESSAGE("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+    MENU_COUNTS_MESSAGE("<주문 메뉴>"),
+    PRICE_BEFORE_DISCOUNT_MESSAGE("<할인 전 총주문 금액>"),
+    GIVEAWAY_MENU_MESSAGE("<증정 메뉴>"),
+    DISCOUNT_BENEFIT_MESSAGE("<혜택 내역"),
+    TOTAL_DISCOUNT_PRICE_MESSAGE("총혜택 금액"),
+    PRICE_AFTER_DISCOUNT_MESSAGE("할인 후 예상 결제 금액"),
+    EVENT_BADGE_MESSAGE("12월 이벤트 배지"),
+    NOTHING("없음")
+    ;
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,10 +1,56 @@
 package christmas.view;
 
-public class OutputView {
+import static christmas.constant.view.OutputMessage.GIVEAWAY_MENU_MESSAGE;
+import static christmas.constant.view.OutputMessage.MENU_COUNTS_MESSAGE;
+import static christmas.constant.view.OutputMessage.NOTHING;
+import static christmas.constant.view.OutputMessage.PREVIEW_MESSAGE;
+import static christmas.constant.view.OutputMessage.PRICE_BEFORE_DISCOUNT_MESSAGE;
+import static christmas.constant.view.OutputMessage.START_MESSAGE;
 
-    private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+import christmas.constant.menu.Menu;
+import christmas.dto.discount.DiscountResponse;
+import christmas.dto.order.OrderResponse;
+import java.util.Map;
+
+public class OutputView {
 
     public static void printStartMessage() {
         System.out.println(START_MESSAGE);
+    }
+
+    public static void printOrderResponse(OrderResponse orderResponse) {
+        System.out.println(PREVIEW_MESSAGE);
+        System.out.println();
+
+        printMenu(orderResponse.getMenuCounts());
+
+        printPriceBeforeDiscount(orderResponse.getPriceBeforeDiscount());
+
+        printGiveawayMenu(orderResponse.getGiveawayMenu());
+    }
+
+    private static void printMenu(Map<Menu, Integer> menuCounts) {
+        if (menuCounts.isEmpty()) {
+            System.out.println(NOTHING);
+            return;
+        }
+
+        System.out.println(MENU_COUNTS_MESSAGE);
+        menuCounts.forEach((menu, count) -> System.out.printf("%s %d개\n", menu.getFood(), count));
+        System.out.println();
+    }
+
+    private static void printPriceBeforeDiscount(int priceBeforeDiscount) {
+        System.out.println(PRICE_BEFORE_DISCOUNT_MESSAGE);
+        System.out.printf("%d원\n", priceBeforeDiscount);
+    }
+
+    private static void printGiveawayMenu(String giveawayMenu) {
+        System.out.println(GIVEAWAY_MENU_MESSAGE);
+        System.out.println(giveawayMenu);
+    }
+
+    public static void printDiscountResponse(DiscountResponse discountResponse) {
+
     }
 }

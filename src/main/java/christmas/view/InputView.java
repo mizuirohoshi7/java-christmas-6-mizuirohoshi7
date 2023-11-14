7c1773996e376235a363bedd727d69bfc832cd45
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-11-14 17:46:58 +0900

    fix(InputView): 메세지를 출력하도록 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,8 +6,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String VISIT_DATE_READ_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String ORDER_READ_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
     public static int readVisitDate() {
         try {
+            System.out.println(VISIT_DATE_READ_MESSAGE);
             return Integer.parseInt(Console.readLine());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_VISIT_DATE.getMessage());
@@ -15,6 +19,7 @@ public class InputView {
     }
 
     public static String readOrder() {
+        System.out.println(ORDER_READ_MESSAGE);
         return Console.readLine();
     }
 }

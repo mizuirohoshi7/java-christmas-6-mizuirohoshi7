b17cdbcf0197c2150b0d65e39f6c36953b484511
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-11-14 18:02:25 +0900

    feat(OutputView): 모든 출력 메세지

--- src/main/java/christmas/constant/view/OutputMessage.java
+++ src/main/java/christmas/constant/view/OutputMessage.java
@@ -8,10 +8,10 @@ public enum OutputMessage {
     PRICE_BEFORE_DISCOUNT_MESSAGE("<할인 전 총주문 금액>"),
     GIVEAWAY_MENU_MESSAGE("<증정 메뉴>"),
     DISCOUNT_BENEFIT_MESSAGE("<혜택 내역"),
-    TOTAL_DISCOUNT_PRICE_MESSAGE("총혜택 금액"),
-    PRICE_AFTER_DISCOUNT_MESSAGE("할인 후 예상 결제 금액"),
-    EVENT_BADGE_MESSAGE("12월 이벤트 배지"),
-    NOTHING("없음")
+    TOTAL_DISCOUNT_PRICE_MESSAGE("<총혜택 금액>"),
+    PRICE_AFTER_DISCOUNT_MESSAGE("<할인 후 예상 결제 금액>"),
+    EVENT_BADGE_MESSAGE("<12월 이벤트 배지>"),
+    NOTHING("<없음>")
     ;
 
     private final String message;
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,15 +1,21 @@
 package christmas.view;
 
+import static christmas.constant.view.OutputMessage.DISCOUNT_BENEFIT_MESSAGE;
+import static christmas.constant.view.OutputMessage.EVENT_BADGE_MESSAGE;
 import static christmas.constant.view.OutputMessage.GIVEAWAY_MENU_MESSAGE;
 import static christmas.constant.view.OutputMessage.MENU_COUNTS_MESSAGE;
 import static christmas.constant.view.OutputMessage.NOTHING;
 import static christmas.constant.view.OutputMessage.PREVIEW_MESSAGE;
+import static christmas.constant.view.OutputMessage.PRICE_AFTER_DISCOUNT_MESSAGE;
 import static christmas.constant.view.OutputMessage.PRICE_BEFORE_DISCOUNT_MESSAGE;
 import static christmas.constant.view.OutputMessage.START_MESSAGE;
+import static christmas.constant.view.OutputMessage.TOTAL_DISCOUNT_PRICE_MESSAGE;
 
+import christmas.constant.discount.DiscountType;
 import christmas.constant.menu.Menu;
 import christmas.dto.discount.DiscountResponse;
 import christmas.dto.order.OrderResponse;
+import java.text.NumberFormat;
 import java.util.Map;
 
 public class OutputView {
@@ -51,6 +57,39 @@ public class OutputView {
     }
 
     public static void printDiscountResponse(DiscountResponse discountResponse) {
+        printDiscountBenefit(discountResponse.getDiscountPriceByType());
 
+        printTotalDiscountPrice(discountResponse.getTotalDiscountPrice());
+
+        printPriceAfterDiscount(discountResponse.getPriceAfterDiscount());
+
+        printEventBadge(discountResponse.getEventBadge());
+    }
+
+    private static void printDiscountBenefit(Map<DiscountType, Integer> discountPriceByType) {
+        System.out.println(DISCOUNT_BENEFIT_MESSAGE);
+        discountPriceByType.forEach((discountType, discountPrice) ->
+                System.out.printf(
+                        "%s: -%s원\n",
+                        discountType.getName(), NumberFormat.getInstance().format(discountPrice))
+        );
+        System.out.println();
+    }
+
+    private static void printTotalDiscountPrice(int totalDiscountPrice) {
+        System.out.println(TOTAL_DISCOUNT_PRICE_MESSAGE);
+        System.out.printf("-%s원\n", NumberFormat.getInstance().format(totalDiscountPrice));
+        System.out.println();
+    }
+
+    private static void printPriceAfterDiscount(int priceAfterDiscount) {
+        System.out.println(PRICE_AFTER_DISCOUNT_MESSAGE);
+        System.out.printf("%s원\n", NumberFormat.getInstance().format(priceAfterDiscount));
+        System.out.println();
+    }
+
+    private static void printEventBadge(String eventBadge) {
+        System.out.println(EVENT_BADGE_MESSAGE);
+        System.out.println(eventBadge);
     }
 }
